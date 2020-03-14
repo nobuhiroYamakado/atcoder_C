6d3e3699b89949f04056e9c3d01212615403f319
nobuhiro yamakado <nobuhiro.yamakado@gmail.com>
2020-03-14 21:07:05 +0900

    wakaran

--- Panasonic2020/A.c
+++ Panasonic2020/A.c
@@ -3,7 +3,9 @@
 
 int main(void)
 {
-    int a;
-    scanf("$d",a);
+    int k;
+    int arr[33] = {1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51};
+    scanf("$d",&k);
+    printf("%d", arr[k+1]);
     return (0);
 }
