bd136c471362efb23e62638cb51bd9a29b7a87d6
ymkd <nobuhiro.yamakado@gmail.com>
2020-03-14 21:29:17 +0900

    wakarang

--- Panasonic2020/B.c
+++ Panasonic2020/B.c
@@ -6,7 +6,14 @@ int main(void)
     long long int h, w;
 
     scanf("%lld %lld", &h, &w);
-    h = ((h/2) * w) + ((h%2) * (w%2));
-    printf("%lld", h);
+    if (h%2 == 1 && w%2 ==1)
+	{
+		h = ((h * w) / 2) + 1;
+	}
+	else 
+	{
+		h = (h * w)/2;
+	}
+	printf("%lld", h);
     return (0);
 }
