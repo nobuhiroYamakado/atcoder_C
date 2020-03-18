622e62b401e35cea2322fa332f7a5b8fb8e1958e
ymkd <nobuhiro.yamakado@gmail.com>
2020-03-18 17:30:55 +0900

    without check delimiter then AC

--- ABC158/p_d.c
+++ ABC158/p_d.c
@@ -3,61 +3,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-char	*strrev(char *str)
-{
-	char *p1, *p2;
-
-	if (!str || !*str)
-		return (str);
-	for (p1 = str, p2 = str + strlen(str) -1; p2 > p1; ++p1, --p2)
-	{
-		*p1 ^= *p2;
-		*p2 ^= *p1;
-		*p1 ^= *p2;
-	}
-	return (str);
-}
-
-char *add_back(char *str, char c)
-{
-	int index = 0;
-	while (str[index] != '\0')
-		index++;
-	str[index] = c;
-	str[index +1] = '\0';
-	return str;
-}
-
-char *add_front(char *str, char c)
-{
-	int index = 0;
-	while (str[index] != '\126')
-		index++;
-	str[index] = c;
-	str[index -1] = '\126';
-	return str;
-}
-
 int		main(void)
 {
-	char str[100001];
 	char buf_str[500002];
 	int q;
-
-	scanf("%s", str);
+	int start_h = 230001;
+	int end_h = start_h;
+	scanf("%s", &buf_str[start_h]);
 	scanf("%d", &q);
-	memset(buf_str, '0', 500002);
-	buf_str[500001] = '\0';
 
-	int index = 0;
-	
-	while (str[index] != '\0')
-	{
-		buf_str[230001 + index] = str[index];
-		index++;
-	}
-	buf_str[230001+index] = '\0';
-	buf_str[230000] = '\126';
+	int index = start_h;
+	while (buf_str[end_h] != '\0')
+		end_h++;
+	start_h = start_h - 1;
 
 	int dq;
 	int f;
@@ -76,29 +34,29 @@ int		main(void)
 		{
 			scanf("%d %c", &f, &c);
 			if ((f == 1 && flag == 1) || (f == 2 && flag == -1))
-				add_front(buf_str, c);
+			{
+				buf_str[start_h] = c;
+				start_h = start_h -1;
+			}
 			else if ((f == 2 && flag ==1) || (f == 1 && flag == -1))
-				add_back(buf_str, c);
+			{
+				buf_str[end_h] = c;
+				buf_str[end_h+1] = '\0';
+				end_h++;
+			}
 		}
 		index++;
 	}
 	index = 0;
-	while (buf_str[index] != '\126')
-		index++;
-	char *r_str = &buf_str[index +1];
+	char *r_str = &buf_str[start_h +1];
 	if (flag == -1)
 	{
-		index = 500000;
-		while (buf_str[index] != '\0')
+		while (index < (end_h - start_h - 1))
 		{
-			index = index - 1;
+			putchar_unlocked(buf_str[end_h-1-index]);
+			index++;
 		}
-		index = index - 1;
-		while (buf_str[index] != '\126')
-		{
-			putchar_unlocked(buf_str[index]);
-			index = index -1;
-		}
+		putchar_unlocked('\n');
 	}
 	else
 		printf("%s\n",r_str);
