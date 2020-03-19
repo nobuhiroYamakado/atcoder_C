fdd9f3324ea89f8a09b175c90046c9664caa1f49
ymkd <nobuhiro.yamakado@gmail.com>
2020-03-20 04:20:03 +0900

    AC without no float

--- Panasonic2020/C.c
+++ Panasonic2020/C.c
@@ -6,19 +6,15 @@ int	main(void)
 	long long int a,b,c;
 
 	scanf("%lld %lld %lld", &a, &b, &c);
-
-	if ((a+b)*2 < c)
-	{
-		printf("Yes");
-	}
-	else if (c-a-b < 0)
+	long long int d = c - a - b;
+	if (d <= 0)
 	{
 		printf("No");
 	}
-	else if (pow(a,2)+pow(b,2)+pow(c,2)-2*(a*b)-2*(b*c)-2*(c*a)>0)
-		{
-			printf("Yes");
-		}
+	else if (d*d > 4*a*b)
+	{
+		printf("Yes");
+	}
 	else
 		printf("No");
 
