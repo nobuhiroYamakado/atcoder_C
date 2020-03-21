e76088134ffec6990144130a0a2b8ae9fdc99541
ymkd <nobuhiro.yamakado@gmail.com>
2020-03-21 20:39:25 +0900

    almost blank file

---
+++ AGC043/A.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int	main(void)
+{
+	int i,j,index;
+	char str[];
+
+	scanf("%d", i);
+	scanf("%d",str);
+	printf("Yes");
+	printf("No");
+	return(0);
+}
