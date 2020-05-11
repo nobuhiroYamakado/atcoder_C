fa82fc3ce03ded2310b3bae91986a199d46a87ae
ymkd <nobuhiro.yamakado@gmail.com>
2020-05-11 18:30:11 +0900

    AC

--- EDPC/e.c
+++ EDPC/e.c
@@ -12,17 +12,19 @@ int main(void)
 	
 	lli we[n];
 	lli va[n];
+	lli val_sum = 0;
 	lli index = 0;
 	while (index < n)
 	{
 		scanf("%lld %lld",&(we[index]),&(va[index]));
+		val_sum += va[index];
 		index++;
 	}
-	lli dp[n+1][w+1];
+	lli dp[n+1][val_sum+1];
 	index = 0;
-	while (index < w+1)
+	while (index < val_sum+1)
 	{
-		dp[0][index] = 0;
+		dp[0][index] = 2000*1000*1000;
 		index++;
 	}
 	index = 0;
@@ -37,21 +39,37 @@ int main(void)
 	while (i < n+1)
 	{
 		j = 1;
-		while (j < w + 1)
+		while (j < val_sum + 1)
 		{
-			if (j-we[i-1]>=0 && va[i-1]+dp[i-1][j-we[i-1]]>dp[i-1][j])
+			if (j - va[i-1] >= 0 && dp[i-1][j] > we[i-1] + dp[i-1][j-va[i-1]])
 			{
-				dp[i][j] = va[i-1] + dp[i-1][j-we[i-1]];
-				printf("dp:%lld, i: %lld, j:%lld\n",dp[i][j], i, j);
+				dp[i][j] = dp[i-1][j-va[i-1]] + we[i-1];
+				//printf("dp:%lld, i: %lld, j:%lld\n",dp[i][j], i, j);
+			}
+			else if (j - va[i-1] < 0 && dp[i-1][j] > we[i-1])
+			{
+				dp[i][j] = we[i-1];
+				//printf("dp:%lld, i: %lld, j:%lld\n",dp[i][j], i, j);
 			}
 			else
 			{
 				dp[i][j] = dp[i-1][j];
+				//printf("dp:%lld, i: %lld, j:%lld\n",dp[i][j], i, j);
 			}
 			j++;
 		}
 		i++;
 	}
-	printf("%lld\n",dp[n][w]);
+	index = 0;
+	while (index < val_sum)
+	{
+		if (w < dp[n][index])
+		{
+			printf("%lld\n",index-1);
+			return (0);
+		}
+		index++;
+	}
+	printf("%lld\n",val_sum);
 	return (0);
 }
