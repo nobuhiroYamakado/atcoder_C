a8d3c1122a54c656513db1394a9be251cca66983
ymkd <nobuhiro.yamakado@gmail.com>
2020-05-18 18:14:31 +0900

    AC

--- EDPC/j.c
+++ EDPC/j.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 typedef struct s_table
 {
 	int num_0;//0枚の皿
@@ -24,137 +25,90 @@ double expect(int num_0, int n)
 	return((n*1.0)/(n-r_num));
 }
 
+double f(int c1, int c2, int c3, int N, double ***dp, int ***memo_flag)
+{
+	double fans;
+	if(memo_flag[c1][c2][c3] == 1)
+	{
+		return (dp[c1][c2][c3]);
+	}
+	memo_flag[c1][c2][c3] = 1;
+	fans = 1/(1-((double)(N-c1-c2-c3)/N));
+	//printf("fan:%.4lf, c1:%d, c2:%d, c3:%d, N:%d\n",fans,c1,c2,c3,N);
+	if(c1>0)
+	{
+		fans += f(c1-1,c2,c3,N,dp,memo_flag)*c1/N/(1-(double)(N-c1-c2-c3)/N);
+	}
+	if(c2>0)
+	{
+		fans += f(c1+1,c2-1,c3,N,dp,memo_flag)*c2/N/(1-(double)(N-c1-c2-c3)/N);
+	}
+	if(c3>0)
+	{
+		fans += f(c1,c2+1,c3-1,N,dp,memo_flag)*c3/N/(1-(double)(N-c1-c2-c3)/N);
+	}
+	return (dp[c1][c2][c3]=fans);
+}
 int main(void)
 {
-	int n,i,j,k,l;
-	int num_0, num_1, num_2, num_3;
-	num_0 = 0;
-	num_1 = 0;
-	num_2 = 0;
-	num_3 = 0;
-	scanf("%d", &n);
+	int ***memo_flag;
+	double ***dp;
+	int n;
 	int *a;
-	int sum = 0;
+	scanf("%d",&n);
 	a = (int *)malloc(sizeof(int)*(n+1));
+	int i,j,k,l;
+	int sum;
 	i = 0;
+	j = 0;
+	k = 0;
+	l = 0;
+	
 	while (i < n)
 	{
 		scanf("%d",&(a[i]));
-		sum = sum + a[i];
-		if(a[i] == 0)
-			num_0++;
-		if(a[i] == 1)
-			num_1++;
-		if(a[i] == 2)
-			num_2++;
-		if(a[i] == 3)
-			num_3++;
 		i++;
 	}
-	t_dp *dp;
-	dp = (t_dp *)malloc(sizeof(t_dp)*(sum+1));
-	l = n;
-	dp[0].exp = 1.0;
-	dp[0].num_t = 0;
-	dp[0].table = (t_table *)malloc(sizeof(t_table)*4);
 	i = 0;
-	if (num_1 > 0)
+	j = 0;
+	memo_flag = (int***)malloc(sizeof(int **)*(301));
+	dp = (double ***)malloc(sizeof(double **)*(301));
+	while (i < 301)
 	{
-		dp[0].table[i].num_0 = 1;
-		dp[0].table[i].num_1 = num_1 - 1;
-		dp[0].table[i].num_2 = num_2;
-		dp[0].table[i].num_3 = num_3;
-		
-		dp[0].table[i].times = num_1;
-		i++;
-	}
-	if (num_2 > 0)
-	{
-		dp[0].table[i].num_0 = 0;
-		dp[0].table[i].num_1 = num_1 + 1;
-		dp[0].table[i].num_2 = num_2 - 1;
-		dp[0].table[i].num_3 = num_3;
-
-		dp[0].table[i].times = num_2;
-		i++;
-	}
-	if (num_3 > 0)
-	{
-		dp[0].table[i].num_0 = 0;
-		dp[0].table[i].num_1 = num_1;
-		dp[0].table[i].num_2 = num_2 + 1;
-		dp[0].table[i].num_3 = num_3 - 1;
-		
-		dp[0].table[i].times = num_3;
-		i++;
-	}
-	dp[0].num_t = i;
-	i = 1;
-	k = 0;
-	int sum_times = 0;
-	while (i < sum)
-	{
-		dp[i].exp = 0;
-		dp[i].num_t = 0;
-		dp[i].table = (t_table *)malloc(sizeof(t_table)*3*dp[i-1].num_t);
-		k = 0;
+		memo_flag[i] = (int **)malloc(sizeof(int *)*(301));
+		dp[i] = (double **)malloc(sizeof(double *)*(301));
 		j = 0;
-		sum_times = 0;
-		while (j < dp[i-1].num_t)
+		while (j < 301)
 		{
-			if (dp[i-1].table[j].num_1 > 0)
+			memo_flag[i][j] = (int*)malloc(sizeof(int)*(301));
+			dp[i][j] = (double*)malloc(sizeof(double)*(301));
+			k = 0;
+			while (k < 301)
 			{
-				dp[i].table[k].num_0 = dp[i-1].table[j].num_0 + 1;
-				dp[i].table[k].num_1 = dp[i-1].table[j].num_1 - 1;
-				dp[i].table[k].num_2 = dp[i-1].table[j].num_2;
-				dp[i].table[k].num_3 = dp[i-1].table[j].num_3;
-
-				dp[i].table[k].times = dp[i-1].table[j].num_1;
+				memo_flag[i][j][k] = 0;
+				dp[i][j][k] = 0;
 				k++;
 			}
-			if (dp[i-1].table[j].num_2 > 0)
-			{
-				dp[i].table[k].num_0 = dp[i-1].table[j].num_0;
-				dp[i].table[k].num_1 = dp[i-1].table[j].num_1 + 1;
-				dp[i].table[k].num_2 = dp[i-1].table[j].num_2 - 1;
-				dp[i].table[k].num_3 = dp[i-1].table[j].num_3;
-
-				dp[i].table[k].times = dp[i-1].table[j].num_2;
-				k++;
-			}
-			if (dp[i-1].table[j].num_3 > 0)
-			{
-				dp[i].table[k].num_0 = dp[i-1].table[j].num_0;
-				dp[i].table[k].num_1 = dp[i-1].table[j].num_1;
-				dp[i].table[k].num_2 = dp[i-1].table[j].num_2 + 1;
-				dp[i].table[k].num_3 = dp[i-1].table[j].num_3 - 1;
-
-				dp[i].table[k].times = dp[i-1].table[j].num_3;
-				k++;
-			}
-			dp[i].exp += expect(dp[i-1].table[j].num_0,n) * dp[i-1].table[j].times;
-			//printf("j:%d, i:%d, exp:%f\n",j,i,dp[i].exp);
-			//printf("num_t;%d, times:%d\n",dp[i-1].num_t, dp[i-1].table[j].times);
-			sum_times += dp[i-1].table[j].times;
 			j++;
 		}
-		dp[i].exp = dp[i].exp / sum_times;
-		dp[i].num_t = k;
-		if(i > 2)
-		{
-			free(dp[i-2].table);
-		}
 		i++;
 	}
 	i = 0;
-	
-	double r_num = expect(1,5);
-	r_num = 0;
-	while (i < sum)
+	j = 0;
+	k = 0;
+	int c[4];
+	c[0] = 0;
+	c[1] = 0;
+	c[2] = 0;
+	c[3] = 0;
+	while (i<n)
 	{
-		r_num += dp[i].exp;
+		c[a[i]]++;
 		i++;
 	}
-	printf("%.10lf\n",r_num);
+	memo_flag[0][0][0] = 1;
+	double ans = f(c[1],c[2],c[3],n,dp,memo_flag);
+	//printf("c1:%d, c2:%d, c3:%d, n;%d\n",c[1],c[2],c[3],n);
+	printf("%.14lf\n",ans);
 	return (0);
 }
